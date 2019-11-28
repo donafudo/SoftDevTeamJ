a51048770f9edc2977aa63a1fa043fe61711774e
Nakai Keitaro <unknown>
2019-11-28 12:13:35 +0900

    初期画面と遷移画面を作った

---
+++ CourseRegistrationManager/CourseRegistrationManager/CreditsEditor.swift
@@ -0,0 +1,21 @@
+//
+//  CreditsEditor.swift
+//  CourseRegistrationManager
+//
+//  Created by Nakai Keitaro on 2019/11/28.
+//  Copyright © 2019 Nakai Keitaro. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreditsEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreditsEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsEditor()
+    }
+}
---
+++ CourseRegistrationManager/CourseRegistrationManager/PersonalEditor.swift
@@ -0,0 +1,21 @@
+//
+//  PersonalEditor.swift
+//  CourseRegistrationManager
+//
+//  Created by Nakai Keitaro on 2019/11/28.
+//  Copyright © 2019 Nakai Keitaro. All rights reserved.
+//
+
+import SwiftUI
+
+struct PersonalEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PersonalEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        PersonalEditor()
+    }
+}
