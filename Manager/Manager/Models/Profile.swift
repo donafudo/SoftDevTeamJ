f841ef70ac979355ab33e75c6092aa677f6740c6
Nakai Keitaro <>
2019-12-13 11:51:55 +0900

    実行時エラーを消した

--- Manager/Manager/Models/Profile.swift
+++ Manager/Manager/Models/Profile.swift
@@ -16,4 +16,12 @@ struct Profile {
     var grade: Int
     
     static let `default` = Self(username: "Nakai", faculty: "工学部", department: "工学科", course: "知能情報", grade: 1)
+    
+    init(username: String, faculty: String, department: String, course: String, grade: Int) {
+        self.username = username
+        self.faculty = faculty
+        self.department = department
+        self.course = course
+        self.grade = grade
+    }
 }
